aec336f64a2a6f2efff0b74d4ac837fc7b92925a
Aravind <aravindthulasiraman@gmail.com>
2025-02-19 19:03:05 +0530

    Commiting header section -css and jsx components

---
+++ frontend/my todo-app/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import "./App.css";
+import TaskForm from './components/TaskForm';
+
+
+const App = () => {
+  return (
+    <div className='app'>
+        <TaskForm></TaskForm>
+      <header className='app_header'>Header Section</header>
+      <main className='app_main'>
+        <section className='task_column'>Section 1</section>
+        <section className='task_column'>Section 2</section>
+        <section className='task_column'>Section 3</section>
+      </main>
+    </div>
+  )
+}
+
+export default App
