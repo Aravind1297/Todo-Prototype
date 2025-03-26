4b51df0e329e5cb61eb756573fcf6d6651d1dd52
Aravind <aravindthulasiraman@gmail.com>
2025-03-26 20:56:37 +0530

    V1.0: CRUD operations with in-memory H2 database and Spring Security enabled. API Authentication Required. Tested with Postman

---
+++ taskmanager/src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,26 @@
+package com.example.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Task{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
---
+++ taskmanager/src/main/java/com/example/taskmanager/service/TaskService.java
@@ -0,0 +1,45 @@
+package com.example.taskmanager.service;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService{
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task){
+        task.setCreatedAt(LocalDateTime.now());
+        task.setUpdatedAt(LocalDateTime.now());
+        return taskRepository.save(task);
+    }
+
+    public Optional<Task> getTaskById(Long id){
+        return taskRepository.findById(id);
+    }
+
+    public Task updateTask(Long id,Task updatedTask){
+        return taskRepository.findById(id).map(task -> {
+            task.setTitle(updatedTask.getTitle());
+            task.setDescription(updatedTask.getDescription());
+            task.setCompleted(updatedTask.isCompleted());
+            task.setUpdatedAt(LocalDateTime.now());
+            return taskRepository.save(task);
+        }).orElseThrow(()->new RuntimeException("Task not Found"));
+    }
+
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id);
+    }
+}
