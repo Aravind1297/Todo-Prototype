493933f2d86807ea39823ee471a5bbc4b288c749
Aravind <aravindthulasiraman@gmail.com>
2025-04-02 15:58:46 +0530

    external database Integrated

--- backend/src/main/java/com/example/taskmanager/controller/TaskController.java
+++ backend/src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -4,16 +4,24 @@ import com.example.taskmanager.model.Task;
 import com.example.taskmanager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     @Autowired
     private TaskService taskService;
+
 
     @GetMapping
     public List<Task> getAllTasks(){
@@ -21,8 +29,11 @@ public class TaskController {
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task){
-        return taskService.createTask(task);
+    public ResponseEntity<?> createTask(@Valid @RequestBody Task task){
+
+        logger.info("Creating a new task: {}",task.getTitle()); // Log the incoming request
+        Task savedTask = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
 
     @GetMapping("/{id}")
--- backend/src/main/java/com/example/taskmanager/model/Task.java
+++ backend/src/main/java/com/example/taskmanager/model/Task.java
@@ -1,6 +1,8 @@
 package com.example.taskmanager.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,8 +18,11 @@ public class Task{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title is required")
     private String title;
+    @NotBlank(message = "Description is required")
     private String description;
+    @NotNull(message = "completed status is required")
     private boolean completed;
 
     private LocalDateTime createdAt;
